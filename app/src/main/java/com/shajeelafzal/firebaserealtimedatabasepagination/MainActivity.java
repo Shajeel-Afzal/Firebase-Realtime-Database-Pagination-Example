f3e961955b88659256e9224470d166661fd7720d
Shajeel_Afzal <shajeel.afzal@gmail.com>
2017-12-13 18:45:54 +0500

    Initial Commit!

---
+++ app/src/main/java/com/shajeelafzal/firebaserealtimedatabasepagination/MainActivity.java
@@ -0,0 +1,13 @@
+package com.shajeelafzal.firebaserealtimedatabasepagination;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
