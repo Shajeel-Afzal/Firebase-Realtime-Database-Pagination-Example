acae5ec43e67c937bbcc341d03d59b1ad7c8bcb5
Shajeel_Afzal <shajeel.afzal@gmail.com>
2017-12-13 20:54:56 +0500

    Pagination Setup.

--- app/src/main/java/com/shajeelafzal/firebaserealtimedatabasepagination/MainActivity.java
+++ app/src/main/java/com/shajeelafzal/firebaserealtimedatabasepagination/MainActivity.java
@@ -1,14 +1,30 @@
 package com.shajeelafzal.firebaserealtimedatabasepagination;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.shajeelafzal.firebaserealtimedatabasepagination.adapters.UserRVAdapter;
+import com.shajeelafzal.firebaserealtimedatabasepagination.models.UserModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity {
 
     private RecyclerView mRV;
+    private UserRVAdapter mAdapter;
+    private int mTotalItemCount = 0;
+    private int mLastVisibleItemPosition;
+    private boolean mIsLoading = false;
+    private int mPostsPerPage = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +39,61 @@ public class MainActivity extends AppCompatActivity {
                 mLayoutManager.getOrientation());
         mRV.addItemDecoration(dividerItemDecoration);
 
+        mAdapter = new UserRVAdapter();
+        mRV.setAdapter(mAdapter);
+
+        getUsers(null);
+
+        mRV.addOnScrollListener(new RecyclerView.OnScrollListener() {
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                mTotalItemCount = mLayoutManager.getItemCount();
+                mLastVisibleItemPosition = mLayoutManager.findLastVisibleItemPosition();
+
+                if (!mIsLoading && mTotalItemCount <= (mLastVisibleItemPosition + mPostsPerPage)) {
+                    getUsers(mAdapter.getLastItemId());
+                    mIsLoading = true;
+                }
+            }
+        });
+    }
+
+    private void getUsers(String nodeId) {
+        Query query;
+
+        if (nodeId == null)
+            query = FirebaseDatabase.getInstance().getReference()
+                    .child(Consts.FIREBASE_DATABASE_LOCATION_USERS)
+                    .orderByKey()
+                    .limitToFirst(mPostsPerPage);
+        else
+            query = FirebaseDatabase.getInstance().getReference()
+                    .child(Consts.FIREBASE_DATABASE_LOCATION_USERS)
+                    .orderByKey()
+                    .startAt(nodeId)
+                    .limitToFirst(mPostsPerPage);
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                UserModel user;
+                List<UserModel> userModels = new ArrayList<>();
+                for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                    userModels.add(userSnapshot.getValue(UserModel.class));
+                }
+
+                mAdapter.addAll(userModels);
+                mIsLoading = false;
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                mIsLoading = false;
+            }
+        });
     }
 }
+
