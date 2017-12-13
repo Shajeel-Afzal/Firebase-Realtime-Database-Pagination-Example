72fe16ccd9431e1a789ef2d1ee175844f4f6dbc1
Shajeel_Afzal <shajeel.afzal@gmail.com>
2017-12-13 19:42:23 +0500

    Adds RecyclerView in the Layout and Attaches it in the MainActivity

--- app/src/main/java/com/shajeelafzal/firebaserealtimedatabasepagination/MainActivity.java
+++ app/src/main/java/com/shajeelafzal/firebaserealtimedatabasepagination/MainActivity.java
@@ -1,13 +1,27 @@
 package com.shajeelafzal.firebaserealtimedatabasepagination;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRV = findViewById(R.id.rv);
+        final LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+        mRV.setLayoutManager(mLayoutManager);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(mRV.getContext(),
+                mLayoutManager.getOrientation());
+        mRV.addItemDecoration(dividerItemDecoration);
+
     }
 }
